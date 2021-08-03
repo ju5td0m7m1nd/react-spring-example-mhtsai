95aa7a9b17011add9b9e3f6da258d5d5db7a1cef
Frank Tsai <franks0059@gmail.com>
2021-08-04 03:08:45 +0800

    feat: step04 chain

---
+++ pages/04.tsx
@@ -0,0 +1,81 @@
+import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+import React, { useCallback, useState } from 'react';
+import { animated, useChain, useSpring, useSpringRef, useTransition } from 'react-spring';
+import BeforeAfter from '../containers/BeforeAfter';
+
+
+function Before() {
+  const [page, setPage] = useState<'app' | 'thankyou'>('app');
+
+  return (
+    <Flex bg='gray' alignItems="center" justifyContent="center" w="full" height="full">
+      {
+        page === "app" && <Flex w="full" h="full" bg="black">
+          <Button onClick={() => setPage('thankyou')} m="auto">Finish App</Button>
+        </Flex>
+      }
+       {
+        page === "thankyou" && <Heading color="#fff">Thank you</Heading>
+      }
+    </Flex>
+  )
+}
+
+function After() {
+  const [page, setPage] = useState<'app' | 'thankyou'>('app');
+
+  const boxShrinkRef = useSpringRef();
+
+  const style = useSpring({
+    ref: boxShrinkRef,
+    from: {
+      size: '100%',
+    },
+    to: {
+      size: '0%',
+    }
+  })
+
+  const textRef = useSpringRef();
+
+  const textStyle = useSpring({
+    ref: textRef,
+    from: {
+      marginTop: '100px',
+      opacity: 0,
+    },
+    to: {
+      marginTop: '0px',
+      opacity: 1,
+    }
+  })
+
+
+  useChain(page === 'app' ? []: [boxShrinkRef, textRef], [0, 1])
+
+  return (
+    <Flex bg='gray' alignItems="center" justifyContent="center" w="full" height="full" position="relative">
+      <animated.div style={{ width: style.size, height: style.size, overflow: 'hidden', position: 'absolute'}}>
+        <Flex w="full" h="full" bg="black">
+          <Button onClick={() => setPage('thankyou')} m="auto">Finish App</Button>
+        </Flex>
+      </animated.div>
+      <animated.div style={textStyle}>
+        <Heading color="#fff">Thank you</Heading>
+      </animated.div>
+    </Flex>
+  )
+}
+
+function Section04() {
+  
+  return (
+    <BeforeAfter  
+      before={<Before />}
+      after={<After />}
+      title="Chain"
+    />
+  )
+}
+
+export default Section04
